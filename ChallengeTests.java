c24f1ce358a2eb46ddb80db6144e67e75e7bb8b6
Lucas Flores <80061839+LucasFlores-tec@users.noreply.github.com>
2022-03-30 22:52:52 -0300

    Add files via upload

---
+++ ChallengeTests.java
@@ -0,0 +1,32 @@
+package test_vcamp;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ChallengeTests {
+
+	  @Test
+	  public void test1() {
+	    assertEquals("Player 2 wins", Challenge.rps("rock", "paper"));
+	  }
+			
+		@Test
+	  public void test2() {
+	    assertEquals("Player 1 wins", Challenge.rps("paper", "rock"));
+	  }
+		
+		@Test
+	  public void test3() {
+	    assertEquals("Player 2 wins", Challenge.rps("paper", "scissors"));
+	  }
+		
+		@Test
+	  public void test4() {
+	    assertEquals("TIE", Challenge.rps("scissors", "scissors"));
+	  }
+		
+		@Test
+	  public void test5() {
+	    assertEquals("Player 1 wins", Challenge.rps("scissors", "paper"));
+	  }
+}
