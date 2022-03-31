9522aa67a890f8fe7b074b7065d3f6da3c665eb0
Lucas Flores <80061839+LucasFlores-tec@users.noreply.github.com>
2022-03-30 22:50:11 -0300

    Add files via upload

---
+++ Challenge.java
@@ -0,0 +1,42 @@
+package test_vcamp;
+
+public class Challenge {
+
+// Stati torna a variável visível para as demais instâncias, nesse caso específicamente a "ChallengeTeste".
+	static String result;
+	
+// O método "rps" recebe dois parâmetros de tipo String, e verifica-os atrávez de um laço de repetição "while",
+// retornando o resultado.
+	public static String rps(String s1, String s2){
+		while(true) {
+			if (s1.equals(s2)){
+				result = "TIE";
+				break;
+			} else if(s1.equals("rock") && s2.equals("paper")){
+				result = "Player 2 wins";
+				break;
+			}
+			else if(s1.equals("paper") && s2.equals("rock")){
+				result = "Player 1 wins";
+				break;
+			}
+			else if(s1.equals("rock") && s2.equals("scissors")){
+				result = "Player 1 wins";
+				break;
+			}
+			else if(s1.equals("scissors") && s2.equals("rock")){
+				result = "Player 2 wins";
+				break;
+			}
+			else if(s1.equals("paper") && s2.equals("scissors")){
+				result = "Player 2 wins";
+				break;
+			}
+			else if(s1.equals("scissors") && s2.equals("paper")){
+				result = "Player 1 wins";
+				break;
+			}
+		}
+		return result;
+	}
+}
